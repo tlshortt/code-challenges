44b0a503c6762f3a9d28f6d787b688bba1197e31
Tim Shortt <tshortt@redroverk12.com>
2025-09-30 21:45:14 -0400

    make fareService more readable

--- septa-fare-calculator/src/services/faresService.ts
+++ septa-fare-calculator/src/services/faresService.ts
@@ -12,18 +12,49 @@ export const useFaresService = (url: string): {
     error: unknown;
 } => {
   const { data, loading, error } = useGetFares(url);
+    if (!data) {
+        return { zoneOptions: [], timeOptions: [], purchaseTypeOptions: [], loading, error };
+    }
 
-  console.log("raw fares:", data);
-
-
-  /* in the below code, the zones[0] part is a hack, based on the assumption that the type and purchase 
+    /* in the below code, the zones[0] part is a hack, based on the assumption that the type and purchase 
     attributes in the json are represented the same way for all zones, and that they are all there.
     leaving it for now, because I need to get to the actual calculation part. */
-  return { 
-    zoneOptions: data ? uniq(data.zones.map((zone: { name: string; }) => zone.name )) : [], 
-    timeOptions: data ? uniq(data.zones[0].fares.map((fare: { type: string }) => fare.type)) : [], 
-    purchaseTypeOptions: data ? uniq(data.zones[0].fares.map((fare: { purchase: string }) => fare.purchase)) : [], 
-    loading, 
-    error 
-  };
+    return { 
+        zoneOptions: getZoneOptions(data.zones), 
+        timeOptions: getTimeOptions(data.zones[0].fares), 
+        purchaseTypeOptions: getPurchaseTypeOptions(data.zones[0].fares), 
+        loading, 
+        error 
+    };
 };
+
+export enum TravelTime {
+    weekday = "Weekday",
+    evening_weekend = "Evening or Weekend",
+    anytime = "Anytime"
+}
+
+export enum PurchaseType {
+    advance_purchase = "Station Kiosk",
+    onboard_purchase = "Onboard"
+}
+
+type Zone = {
+  name: string;
+  fares: Fare[];
+};
+
+type Fare = {
+  type: keyof typeof TravelTime;
+  purchase: keyof typeof PurchaseType;
+};
+
+// --- helpers --- //
+const getZoneOptions = (zones: Zone[]) =>
+  uniq(zones.map((z) => z.name));
+
+const getTimeOptions = (fares: Fare[]) =>
+  uniq(fares.map((f) => TravelTime[f.type]));
+
+const getPurchaseTypeOptions = (fares: Fare[]) =>
+  uniq(fares.map((f) => PurchaseType[f.purchase]));
